b2947d7d37d8a988f57cbf789bedc757c18aa208
Muhammad Usman <51905035+muhammadusmanuet@users.noreply.github.com>
2020-08-20 16:13:56 +0500

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+
+<footer class="page-footer font-small mt-3">
+    <hr>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-1">© 2020 Copyright: Muhammad Usman
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</body>
---
+++ templates/header.php
@@ -0,0 +1,44 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usman Pizza</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <style type="text/css">
+        body {
+            font-family: 'Lato', sans-serif;
+            ;
+        }
+
+        .btn {
+            background-color: #cbb09c !important;
+            color: white;
+            border-radius: 0 !important;
+        }
+
+        .btn:hover {
+            color: white !important;
+        }
+        .navbar {
+            min-height: 80px !important;
+        }
+
+        .navbar-brand {
+            font-size: 30px;
+            font-weight: lighter !important;
+            color: #cbb09c !important;
+        }
+        .mt-n5 {
+            margin-top: -4rem !important;
+        }
+
+    </style>
+</head>
+
+<body class="bg-light">
+    <nav class="navbar navbar-light bg-white justify-content-between mb-3">
+        <div class="container">
+            <a class="navbar-brand " href="index.php">Usman Pizza</a>
+            <a href="add.php" class="btn text-uppercase">Add a pizza</a>
+        </div>
+    </nav>
